675668d1f9e03de234b9de52dbc9e982af0a8ba1
ulisesirigoyen <fulises.irigoyen@outlook.com>
2025-09-05 15:40:00 -0400

    Key errors fixed, input text is cleaned after input.

--- web.py
+++ web.py
@@ -3,8 +3,10 @@ import functions
 
 def add_todo():
     todo = st.session_state['new_todo'] + '\n'
-    todos.append(todo)
-    functions.write_todos(todos)
+    if todo:
+        todos.append(todo)
+        functions.write_todos(todos)
+    st.session_state['new_todo'] = ""
 
 todos = functions.get_todos()
 
@@ -13,11 +15,12 @@ st.subheader('This is my todo app')
 st.write('This app is to increase your productivity')
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    key = f'todo-{index}'
+    checkbox = st.checkbox(todo, key=key)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+        del st.session_state[key]
         st.rerun()
 
 st.text_input(label='Enter a new todo:',
